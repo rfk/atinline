6ad6e5c6a57eca290e121b0bc136672692e43beb
Ryan Kelly <ryan@rfk.id.au>
2011-04-13 09:37:40 +1000

    adding setup.py and associated files

--- atinline/__init__.py
+++ atinline/__init__.py
@@ -61,6 +61,12 @@ module, which is atinline's only dependency.
 
 """
 
+__ver_major__ = 0
+__ver_minor__ = 1
+__ver_patch__ = 0
+__ver_sub__ = ""
+__version__ = "%d.%d.%d%s" % (__ver_major__,__ver_minor__,__ver_patch__,__ver_sub__)
+
 
 import sys
 import new
